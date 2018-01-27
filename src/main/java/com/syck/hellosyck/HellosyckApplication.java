419463f4f87cc31ab00efdf8d043c49f3afe80d1
Mark Pantojan <mpantojan@gmail.com>
2018-01-26 21:52:38 -0500

    First Commit

---
+++ src/main/java/com/syck/hellosyck/HellosyckApplication.java
@@ -0,0 +1,13 @@
+package com.syck.hellosyck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellosyckApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HellosyckApplication.class, args);
+	}
+}
