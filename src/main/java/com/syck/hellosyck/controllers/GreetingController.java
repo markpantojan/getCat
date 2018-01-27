650a355b916fc00635f46fe708df6b039c2a6d90
Mark Pantojan <mpantojan@gmail.com>
2018-01-27 00:46:08 -0500

    Added CORS for testing

--- src/main/java/com/syck/hellosyck/controllers/GreetingController.java
+++ src/main/java/com/syck/hellosyck/controllers/GreetingController.java
@@ -12,6 +12,7 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/greeting/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Greeting greeting(@PathVariable("name") String name){
         System.out.println("In greeting method, name is --> " + name);
