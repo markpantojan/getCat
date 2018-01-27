d24bc7998f8edfffaefb937d1516a9c731115e9e
Mark Pantojan <mpantojan@gmail.com>
2018-01-26 22:15:47 -0500

    Added hood page

--- src/main/java/com/syck/hellosyck/HellosyckApplication.java
+++ src/main/java/com/syck/hellosyck/HellosyckApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HellosyckApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(HellosyckApplication.class, args);
 	}
+
 }
--- src/main/java/com/syck/hellosyck/controllers/IndexController.java
+++ src/main/java/com/syck/hellosyck/controllers/IndexController.java
@@ -11,5 +11,10 @@ public class IndexController {
         return "index";
     }
 
+    @RequestMapping("/hood")
+    public String hood(){
+        return "hood";
+    }
+
 
 }
