f14ad44153790bd3d4cb8a93287300b1f5ed24a9
Mark Pantojan <mpantojan@gmail.com>
2018-01-26 23:44:07 -0500

    Added console log to test if Heroku is pulling on git push

--- src/main/java/com/syck/hellosyck/controllers/GreetingController.java
+++ src/main/java/com/syck/hellosyck/controllers/GreetingController.java
@@ -15,7 +15,7 @@ public class GreetingController {
     @RequestMapping(value = "/greeting/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Greeting greeting(@PathVariable("name") String name){
         System.out.println("In greeting method, name is --> " + name);
-                
+        System.out.println("Checking if Heroku is pulling from git");
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
